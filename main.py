e9f0c0bce61784c9ab7ddd61d61b078e9eec06f2
artificial-lua <artificialluamail@gmail.com>
2022-05-04 09:41:41 +0900

    add submodule

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
 if __name__ == '__main__':
     print('main - run')
     
+    try:
+        from submodule import submodule
+        print(f'submodule - {submodule.version_check()}')
+    except ImportError:
+        print('main - import error')
