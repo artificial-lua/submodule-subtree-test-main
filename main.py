4e3f1fffeab669c30e30f62f23c5de4ec391c8a6
artificial-lua <artificialluamail@gmail.com>
2022-05-04 10:11:39 +0900

    submodule

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-
+module_list = []
 
 if __name__ == '__main__':
     print('main - run')
     
     try:
         from submodule import submodule
-        print(f'submodule - {submodule.version_check()}')
+        module = submodule.version_check()
+        module_name = module['name']
+        module_version = module['version']
+        print(f'submodule - {module_name} : {module_version}')
+        module_list.append(module)
     except ImportError:
         print('main - import error')
